756991b2f122e3efe4df1d86dd84fdb1651b5a07
Samuel Okeke <samwizzy11@gmail.com>
2025-02-19 16:46:49 +0100

    updated db.json file

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
+
+server.use(cors);
 
 server.use(middlewares);
 
